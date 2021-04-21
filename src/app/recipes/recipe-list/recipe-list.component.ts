94a4a77ffac34023d0d2e6e2f1150cede2445f42
Alex <alex.yeh@mail.utoronto.ca>
2021-04-22 02:50:30 +0900

    recipe component displays content

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('test recipe', 'tester for recipe', 'https://cdn.frankerfacez.com/emoticon/228449/4'),
+    new Recipe('test 2', 'tester 2', 'https://cdn.frankerfacez.com/emoticon/228449/4')
+  ];
 
   constructor() { }
 
