bccf3eec203efb8b2ac939e2833633bb581485e4
Alex <alex.yeh@mail.utoronto.ca>
2021-04-28 06:04:24 +0900

    basic page Navigation implemented

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() featureSelected = new EventEmitter<string>();
+
   collapsed = true;
 
   constructor() { }
@@ -14,4 +16,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSelect(feature: string){
+    this.featureSelected.emit(feature)
+  }
 }
